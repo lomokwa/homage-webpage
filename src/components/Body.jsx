d28448e2bfdb3423b0c714468e3db7cb0469ae0e
Lorenzo Mokwa <lomokwa@Lorenzos-MacBook-Air.local>
2023-07-02 21:22:12 -0400

    added homage page

---
+++ src/components/Body.jsx
@@ -0,0 +1,86 @@
+export default function Body() {
+  return (
+    <main>
+      <header>
+        <section className="headerImage">
+          <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExcHd3b3ViYjRlMnVpZmloOGp5aGE5bXdybnh3ODRvYzV1YTZxcHc2aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/yf5RZfumZScOEGgA61/giphy.gif" alt="" />
+        </section>
+        <h1 className="title">A Tribute to <br/>Radiohead</h1>
+      </header> 
+      
+     
+
+      <section className="album-list">
+        <div>
+          <a href="https://open.spotify.com/album/3gBVdu4a1MMJVMy6vwPEb8?si=HhEtDS8gRfqny8Vy7ah8Dg" target="_blank">
+            <img src="https://m.media-amazon.com/images/I/517JJb1KLPL._UF1000,1000_QL80_.jpg" alt="Radiohead Pablo Honey Album Cover"/>
+          </a>
+          <h2>Pablo Honey</h2>
+        </div>
+
+        <div>
+          <a href="https://open.spotify.com/album/35UJLpClj5EDrhpNIi4DFg?si=u0lYUYlyR0qCvuTuRtVrfg" target="_blank">
+            <img src="https://m.media-amazon.com/images/I/71VJAU9IPSL._UF1000,1000_QL80_.jpg" alt="Radiohead The Bends Album Cover" />
+          </a>
+          <h2>The Bends</h2>
+        </div>
+
+        <div>
+          <a href="" target="_blank">
+            <img src="https://m.media-amazon.com/images/I/715LZJ5qX0L._UF1000,1000_QL80_.jpg" alt="" />
+          </a>
+          <h2>OK Computer</h2>
+        </div>
+
+        <div>
+          <a href="" target="_blank">
+            <img src="https://m.media-amazon.com/images/I/51ZBeKY7gOL._UF1000,1000_QL80_.jpg" alt="" />
+          </a>
+          <h2>Kid A</h2>
+        </div>
+
+        <div>
+          <a href="" target="_blank">
+            <img src="https://m.media-amazon.com/images/I/41tjmuOAWWL._UF1000,1000_QL80_.jpg" alt="" />
+          </a>
+          <h2>Amnesiac</h2>
+        </div>
+
+        <div>
+          <a href="" target="_blank">
+            <img src="https://m.media-amazon.com/images/I/61N0uGVbM3L._UF1000,1000_QL80_.jpg" alt="" />
+          </a>
+          <h2>Hail To the Thief</h2>
+        </div>
+
+        <div>
+          <a href="" target="_blank">
+            <img src="https://m.media-amazon.com/images/I/A1MwaIeBpwL._UF1000,1000_QL80_.jpg" alt="" />
+          </a>
+          <h2>In Rainbows</h2>
+        </div>
+
+        <div>
+          <a href="" target="_blank">
+            <img src="https://m.media-amazon.com/images/I/716MpZxvJcL._UF1000,1000_QL80_.jpg" alt="" />
+          </a>
+          <h2>The King Of Limbs</h2>
+        </div>
+
+        <div>
+          <a href="" target="_blank">
+            <img src="https://m.media-amazon.com/images/I/51N87gPbmnL._UF1000,1000_QL80_.jpg" alt="" />
+          </a>
+          <h2>A Moon Shaped Pool</h2>
+        </div>
+      </section>
+
+      <section className="about">
+        <p>Radiohead is an English Alt-Rock band formed in 1985.</p>
+        <p>The band released their debut album, Pablo Honey, in 1993. Their debut single, "Creep", became a worldwide hit.</p>
+        <p>Radiohead's popularity and critical standing rose with the release of The Bends in 1995. Radiohead's third album, OK Computer (1997), brought them international fame; noted for its complex production and themes of modern alienation, it is acclaimed as a landmark record and one of the best albums in popular music.</p>
+      </section>
+    </main>
+  )
+  
+}
